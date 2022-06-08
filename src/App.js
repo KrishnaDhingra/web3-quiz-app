368c2bc272178c93c53aaa3cbc415311ee5d0b63
Krishna Dhingra <dkrishna1608@gmail.com>
2022-06-08 18:13:41 +0530

    Fetching questions from the api and displayed it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const [quizData, setQuizData] = useState([])
+
+  const URL =
+    'https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple&token=cf674e7b487fe45308e56e61d1dd3bc0fec5435cfae268aa5f92496286ce27ff'
+
+  const getQuizData = async () => {
+    const response = await axios.get(URL)
+    setQuizData(response.data.results[0])
+  }
+
+  function checkIfNewQuiz() {
+    const mins = new Date().getMinutes()
+    if (mins == '00') {
+      getQuizData()
+    }
+    console.log('Tick ' + mins)
+  }
+
+  setInterval(checkIfNewQuiz, 1000 * 30)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Question</h3>
+      <p>{quizData.question}</p>
+
+      <section className="options">
+        {quizData.length !== 0 &&
+          [quizData.correct_answer, ...quizData.incorrect_answers].map(
+            (element, index) => {
+              return <button key={index}>{element}</button>
+            },
+          )}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
